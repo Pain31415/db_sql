642593641fdfad651da9c8d3b1da13bc3facf68f
Chernii Vladyslav Ihorovych <Cherniy412@ukr.net>
2024-04-14 12:38:18 +0200

    Update sql_script.sql

---
+++ sql_script.sql
@@ -0,0 +1,5 @@
+-- Delete the record(s) from the Wards table where Building is 2 and Floor is 3.
+DELETE FROM Wards WHERE Building = 2 AND Floor = 3;
+
+-- Delete the record(s) from the Examinations table where DayOfWeek is 6.
+DELETE FROM Examinations WHERE DayOfWeek = 6;
